db1348049dff77b4431cc7134f9a27aed78a472e
cutiefunny <cutiefunny@gmail.com>
2025-12-02 10:54:38 +0900

    낙서판, 플레이리스트 추가

--- data/entertainment.js
+++ data/entertainment.js
@@ -16,6 +16,20 @@ export const entertainmentItems = [
     link: "https://musclecat-toon.com", // (projects.js의 serviceUrl)
   },
   {
+    id: "drawing",
+    name: "근육고양이 낙서판",
+    description: "근육고양이잡화점의 방명록으로 사용되는 낙서판이다!", // (projects.js의 shortDescription)
+    imageUrl: "/images/drawing-title.jpg", // (projects.js의 imageUrl)
+    link: "https://musclecat-drawing.vercel.app/", // (projects.js의 serviceUrl)
+  },
+  {
+    id: "playlist",
+    name: "근육고양이 플레이리스트",
+    description: "근육고양이잡화점 매장의 배경음악 플레이리스트이다!", // (projects.js의 shortDescription)
+    imageUrl: "/images/playlist-title.png", // (projects.js의 imageUrl)
+    link: "https://musclecat-playlist.vercel.app/", // (projects.js의 serviceUrl)
+  },
+  {
     id: "catsurvival",
     name: "길냥이 서바이벌",
     description: "벌판의 들개들로부터 살아 남자! 뱀서류 길고양이 게임입니다.", // (projects.js의 shortDescription)
